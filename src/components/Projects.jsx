7f51f8709da9fa43b23307163ef650d6255c1409
Mayurimane936 <mayurimane936@gmail.com>
2025-03-30 11:27:36 +0530

    done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,51 +3,55 @@ import { PROJECTS } from '../constants'
 import {motion} from "framer-motion"
 
 const Projects = () => {
-  return (
-    <div className='border-b border-neutral-800 pb-4 p-3'>
-    <motion.h2 
-      whileInView={{opacity:1, y:0}}
-        initial={{opacity:0, y:-100}}
-        transition={{duration:1.5}} 
-    className='my-20 text-center text-4xl'>
-         Projects
-    </motion.h2>
-
-    {PROJECTS.map((project, index) => (
-<div className='flex flex-col items-center'>
-  <div key={index} className='w-full lg:w-1/2 lg:p-8'>
-    <div className='flex flex-col lg:flex-row items-center mb-8'>
-      <motion.div 
-            whileInView={{opacity:1, x:0}}
-            initial={{opacity:0, x:-100}}
-            transition={{duration:1}}
-      className='w-full lg:w-1/2 mb-4 lg:mb-0'>
-        <img 
-          src={project.image} 
-          alt={project.title} 
-          className='opacity-60 rounded-md w-full h-auto object-cover' 
-        />
-      </motion.div>
-      <motion.div 
-        whileInView={{opacity:1, x:0}}
-        initial={{opacity:0, x:100}}
-        transition={{duration:1}}
-      className='w-full lg:w-1/2 lg:pl-8'>
-        <h6 className='text-xl font-semibold'>{project.title}</h6>
-        <p className='text-neutral-600 pb-3'>{project.description}</p>
-        {project.technologies.map((tech, index)=>(
-            <span key={index} className='mr-2 rounded bg-neutral-800 px-2 py-1 text-sm font-medium text-purple-800'>{tech}</span>
-        ))}
-      </motion.div>
-    </div>
-  </div>
-</div>
-
-  
-))}
-    </div>
-      
-  )
+    return (
+        <div className="border-b border-neutral-900 pb-4">
+          <motion.h1
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1.5 }}
+            className="my-20 text-center"
+          >
+            Projects
+          </motion.h1>
+          <div className="">
+            {PROJECTS.map((project, index) => (
+              <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+                <motion.div
+                  whileInView={{ opacity: 1, x: 0 }}
+                  initial={{ opacity: 0, x: -100 }}
+                  transition={{ duration: 1 }}
+                  className="w-full lg:w-1/4"
+                >
+                  <img
+                    src={project.image}
+                    alt={project.title}
+                    className="mb-6 rounded"
+                    width={150}
+                    height={150}
+                  ></img>
+                </motion.div>
+                <motion.div
+                  whileInView={{ opacity: 1, x: 0 }}
+                  initial={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 1 }}
+                  className="w-full max-w-xl lg:w-3/4"
+                >
+                  <h6 className="mb-2 font-semibold">{project.title}</h6>
+                  <p className="mb-4 text-neutral-400">{project.description}</p>
+                  {project.technologies.map((tech, index) => (
+                    <span
+                      key={index}
+                      className="mr-2 rounded bg-neutral-900 py-1 text-sm font-medium text-purple-900"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </motion.div>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
 }
 
 export default Projects
